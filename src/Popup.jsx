d1062f5c522e99448aad1442a59ddf9944db65ae
noliveira95 <noliveira95@outlook.com>
2024-04-28 17:32:30 -0700

    refactor: move displayBookmarks to utils

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -1,6 +1,6 @@
 import './Popup.css';
 import { useEffect } from 'react';
-import { addBookmark, removeBookmark } from './utils';
+import { addBookmark, removeBookmark, displayBookmarks } from './utils';
 
 function Popup() {
   useEffect(() => {
@@ -10,29 +10,6 @@ function Popup() {
       const bookmarkList = document.getElementById('bookmarkList');
       displayBookmarks(tree[0].children, bookmarkList);
     });
-
-    // Recursively display the bookmarks
-    function displayBookmarks(nodes, parentNode) {
-      for (const node of nodes) {
-        // If the node is a bookmark, create a list item and append it to the parent node
-        if (node.url) {
-          const listItem = document.createElement('li');
-          const link = document.createElement('a');
-          link.href = node.url;
-          link.target = '_blank';
-          link.textContent = node.title;
-          listItem.appendChild(link);
-          parentNode.appendChild(listItem);
-        }
-
-        // If the node has children, recursively display them
-        if (node.children) {
-          const sublist = document.createElement('ul');
-          parentNode.appendChild(sublist);
-          displayBookmarks(node.children, sublist);
-        }
-      }
-    }
   }, []);
 
   return (
--- src/utils.js
+++ src/utils.js
@@ -24,3 +24,26 @@ export function removeBookmark() {
     location.reload();
   });
 }
+
+// Recursively display the bookmarks
+export function displayBookmarks(nodes, parentNode) {
+  for (const node of nodes) {
+    // If the node is a bookmark, create a list item and append it to the parent node
+    if (node.url) {
+      const listItem = document.createElement('li');
+      const link = document.createElement('a');
+      link.href = node.url;
+      link.target = '_blank';
+      link.textContent = node.title;
+      listItem.appendChild(link);
+      parentNode.appendChild(listItem);
+    }
+
+    // If the node has children, recursively display them
+    if (node.children) {
+      const sublist = document.createElement('ul');
+      parentNode.appendChild(sublist);
+      displayBookmarks(node.children, sublist);
+    }
+  }
+}
