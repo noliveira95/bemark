bd2ef71ac49144d5c1b6c85bf9ad8e89e3bc5cde
noliveira95 <noliveira95@outlook.com>
2024-04-30 17:12:41 -0700

    refactor: separate folders from bookmarks

--- src/utils.js
+++ src/utils.js
@@ -28,22 +28,34 @@ export function removeBookmark() {
 // Recursively display the bookmarks
 export function displayBookmarks(nodes, parentNode) {
   for (const node of nodes) {
-    // If the node is a bookmark, create a list item and append it to the parent node
     if (node.url) {
-      const listItem = document.createElement('li');
-      const link = document.createElement('a');
-      link.href = node.url;
-      link.target = '_blank';
-      link.textContent = node.title;
-      listItem.appendChild(link);
-      parentNode.appendChild(listItem);
+      displayBookmark(node, parentNode);
+    } else {
+      displayFolder(node, parentNode);
     }
+  }
+}
 
-    // If the node has children, recursively display them
-    if (node.children) {
-      const sublist = document.createElement('ul');
-      parentNode.appendChild(sublist);
-      displayBookmarks(node.children, sublist);
-    }
+export function displayBookmark(node, parentNode) {
+  const listItem = document.createElement('li');
+  const link = document.createElement('a');
+  link.href = node.url;
+  link.target = '_blank';
+  link.textContent = node.title;
+  listItem.appendChild(link);
+  listItem.setAttribute('class', 'bookmark');
+  parentNode.appendChild(listItem);
+}
+
+export function displayFolder(node, parentNode) {
+  const listItem = document.createElement('li');
+  listItem.textContent = node.title;
+  listItem.setAttribute('class', 'folder');
+  parentNode.appendChild(listItem);
+
+  if (node.children) {
+    const sublist = document.createElement('ul');
+    parentNode.appendChild(sublist);
+    displayBookmarks(node.children, sublist);
   }
 }
