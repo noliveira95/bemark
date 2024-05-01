b744c1c61cd1526660dba9ca7b45c34c236e9b33
noliveira95 <noliveira95@outlook.com>
2024-05-01 13:22:22 -0700

    feat: add icons to folders
    Add folder icons to folder component.
    Replace button with div to remove button styles.

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 import { getAllItems } from '../utils';
 import PropTypes from 'prop-types';
+import { IoFolder, IoFolderOpen } from 'react-icons/io5';
 
 function Folder({ title, items }) {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <li className="folder">
-      <button onClick={() => setIsOpen(!isOpen)}>{title}</button>
+      <div>
+        <div className="folder-button" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <IoFolderOpen /> : <IoFolder />}
+          {title}
+        </div>
+      </div>
       <ul className={`folder-items ${isOpen ? 'visible' : 'hidden'}`}>
         {getAllItems(items)}
       </ul>
