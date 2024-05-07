ad022204d4d7b5f7e52722241b21ae311d3270c9
noliveira95 <noliveira95@outlook.com>
2024-05-06 17:16:42 -0700

    refactor: create ItemsList component

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -1,10 +1,10 @@
 import './Popup.css';
 import { useEffect, useState } from 'react';
-import { getAllItems, getBookmarkTree } from './utils/utils';
+import { getBookmarkTree } from './utils/utils';
 import Navbar from './components/Navbar';
 import Search from './components/Search';
 import FavoritesList from './components/FavoritesList';
-import { BsDashSquare } from 'react-icons/bs';
+import ItemsList from './components/ItemsList';
 
 function Popup() {
   const [allItems, setAllItems] = useState([]);
@@ -32,20 +32,7 @@ function Popup() {
     <div id="popup">
       <Navbar />
       <FavoritesList allItems={allItems} />
-      <div className="items-list-title">
-        <h1>All Bookmarks</h1>
-        <div
-          className={`title-actions ${
-            scrollY >= 220 ? 'opacity-1' : 'opacity-0'
-          }`}
-        >
-          <BsDashSquare className="title-actions-button" />
-        </div>
-      </div>
-      {allItems.length === 0 && (
-        <p className="no-items-message">No bookmarks yet!</p>
-      )}
-      <ul className="items-list">{getAllItems(allItems)}</ul>
+      <ItemsList allItems={allItems} scrollY={scrollY} />
       <Search />
     </div>
   );
---
+++ src/components/ItemsList.jsx
@@ -0,0 +1,32 @@
+import '../Popup.css';
+import { getAllItems } from '../utils/utils';
+import { BsDashSquare } from 'react-icons/bs';
+import PropTypes from 'prop-types';
+
+function ItemsList({ allItems, scrollY }) {
+  return (
+    <div className="items-list-container">
+      <div className="items-list-title">
+        <h1>All Bookmarks</h1>
+        <div
+          className={`title-actions ${
+            scrollY >= 220 ? 'opacity-1' : 'opacity-0'
+          }`}
+        >
+          <BsDashSquare className="title-actions-button" />
+        </div>
+      </div>
+      {allItems.length === 0 && (
+        <p className="no-items-message">No bookmarks yet!</p>
+      )}
+      <ul className="items-list">{getAllItems(allItems)}</ul>
+    </div>
+  );
+}
+
+ItemsList.propTypes = {
+  allItems: PropTypes.array.isRequired,
+  scrollY: PropTypes.number.isRequired,
+};
+
+export default ItemsList;
