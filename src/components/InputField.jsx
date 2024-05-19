5dfe334a4542751459dbfd8824a0fbe83d69059e
noliveira95 <noliveira95@outlook.com>
2024-05-18 18:23:35 -0700

    style: init input field styles

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,11 +1,17 @@
+import styles from './styles/InputField.module.css';
 import PropTypes from 'prop-types';
 
 // InputField.jsx
 function InputField({ label, value, onChange }) {
   return (
-    <div>
-      <label>{label}</label>
-      <input type="text" value={value} onChange={onChange} />
+    <div className={styles['input-field-container']}>
+      <label className={styles['input-label']}>{label}</label>
+      <input
+        className={styles['input-field']}
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 }
