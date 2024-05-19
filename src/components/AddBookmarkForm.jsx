9bd298f2b48ad4e5cb4149b28026d6853836fd33
noliveira95 <noliveira95@outlook.com>
2024-05-19 12:30:34 -0700

    feat: add dropdown, init styles

--- src/components/AddBookmarkForm.jsx
+++ src/components/AddBookmarkForm.jsx
@@ -1,20 +1,33 @@
 import styles from './styles/AddBookmarkForm.module.css';
+import { useState } from 'react';
 import Checkbox from './Checkbox';
-// import Dropdown from './Dropdown';
 import InputField from './InputField';
 import Button from './Button';
+import Dropdown from './Dropdown';
 
 function AddBookmarkForm() {
+  const [dropdownValue, setDropdownValue] = useState('category-1');
+
+  const categories = [
+    { label: 'Category 1', value: 'category-1' },
+    { label: 'Category 2', value: 'category-2' },
+    { label: 'Category 3', value: 'category-3' },
+  ];
+
+  function handleCategoryChange(event) {
+    setDropdownValue(event.target.value);
+  }
+
   return (
     <form className={styles['add-bookmark-form']}>
       <InputField label="Title" value={'title'} onChange={null} />
       <InputField label="URL" value={'url'} onChange={null} />
-      {/* <Dropdown
+      <Dropdown
         label="Category"
         options={categories}
-        value={category}
+        value={dropdownValue}
         onChange={handleCategoryChange}
-      /> */}
+      />
       <Checkbox label="Favorite" checked={null} onChange={null} />
       <Button label={'Add Bookmark'} />
     </form>
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,10 +1,15 @@
+import styles from './styles/Dropdown.module.css';
 import PropTypes from 'prop-types';
 
 function Dropdown({ label, options, value, onChange }) {
   return (
-    <div>
-      <label>{label}</label>
-      <select value={value} onChange={onChange}>
+    <div className={styles['dropdown-container']}>
+      <label className={styles['dropdown-label']}>{label}</label>
+      <select
+        className={styles['dropdown-select']}
+        value={value}
+        onChange={onChange}
+      >
         {options.map((option, index) => (
           <option key={index} value={option.value}>
             {option.label}
