8a1251ef2d14df163885ca5059b82c809a76cdb5
noliveira95 <noliveira95@outlook.com>
2024-05-24 17:48:40 -0700

    feat: add input field and button

--- src/screens/AddFolder.jsx
+++ src/screens/AddFolder.jsx
@@ -1,7 +1,15 @@
+import styles from '../components/styles/AddFolderForm.module.css';
+import Button from '../components/Button';
+import InputField from '../components/InputField';
+
 function AddFolder() {
   return (
     <div className="screen">
       <h1>Add Folder</h1>
+      <form className={styles['add-folder-form']}>
+        <InputField label="Folder Name" />
+        <Button label="Add Folder" />
+      </form>
     </div>
   );
 }
